f7a2071ca2236cdcf1851f04551098a08988e469
YunJoon <kpl88@naver.com>
2022-07-27 16:48:19 +0900

    [Modify] Reflection View 수정
    코드리뷰를 기반으로 Reflection View 코드를 수정했습니다

--- TalTal/TalTal/Controllers/ReflectionViewController.swift
+++ TalTal/TalTal/Controllers/ReflectionViewController.swift
@@ -16,31 +16,27 @@ enum MissionType {
 final class ReflectionViewController: UIViewController {
 
     
-    @IBOutlet weak var missionType: UILabel!
+    @IBOutlet weak var missionTypeLabel: UILabel!
     
     @IBOutlet weak var missionLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     
     @IBOutlet weak var reflectionLabel: UILabel!
-    @IBOutlet weak var informationLabel: UILabel!
+    @IBOutlet weak var missionInformationLabel: UILabel!
     
-    // 데이터 전달 받을 시 저장할 프로퍼티
+    // TODO: 아직 더미데이터를 이용 중이라 테이블 뷰에서 넘겨받을 미션 데이터를 저장할 프로퍼티를 미리 만들어 놓았습니다, 이후 꼬마와 데이터를 어떻게 전달받을 지 상의가 끝나면 주석을 해제할 예정입니다.
 //    var type: MissionType?
 //    var missionString: String?
 //    var dateString: String?
 //    var reflectionString: String?
 //    var informationString: String?
-    
+        
     
     // MARK: - viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureUI()
-        
-//        maxTextConfigureUI()
-        
-//        testConfigureUI()
         
     }
     
@@ -54,26 +50,26 @@ final class ReflectionViewController: UIViewController {
         // daily, weekly 구분
         switch mission {
         case .daily:
-            missionType.text = "일일 미션"
-            missionType.backgroundColor = UIColor(named: "PointPink")
+            missionTypeLabel.text = "일일 미션"
+            missionTypeLabel.backgroundColor = UIColor(named: "PointPink")
         case .weekly:
-            missionType.text = "주간 미션"
-            missionType.backgroundColor = UIColor(named: "PointBlue")
+            missionTypeLabel.text = "주간 미션"
+            missionTypeLabel.backgroundColor = UIColor(named: "PointBlue")
         }
         
         // 각 UILabel의 텍스트 설정
         missionLabel.text = "잠자리를 정리하기"
         reflectionLabel.text = "졸렸지만 정리하고 나니 개운하게 하루를 시작할 수 있었다"
-        informationLabel.text = "매일 아침 “잠자리를 정리했다는 것”은 그날의 첫 번째 과업을 달성했다는 뜻입니다. 이 작은 성취감이 또 다른 일을 해낼 수 있겠다는 용기로 발전할 수 있을 것입니다."
+        missionInformationLabel.text = "매일 아침 “잠자리를 정리했다는 것”은 그날의 첫 번째 과업을 달성했다는 뜻입니다. 이 작은 성취감이 또 다른 일을 해낼 수 있겠다는 용기로 발전할 수 있을 것입니다."
         
         // 행간격 조절
         reflectionLabel.setLineSpacing(lineSpacing: 4)
-        informationLabel.setLineSpacing(lineSpacing: 4)
+        missionInformationLabel.setLineSpacing(lineSpacing: 4)
         
     }
     
     
-    // MARK: - Test: 최대 텍스트 UI 생성
+    // MARK: - 최대 텍스트 UI 생성
     private func maxTextConfigureUI() {
         view.backgroundColor = #colorLiteral(red: 0.9725491405, green: 0.9725491405, blue: 0.9725491405, alpha: 1)
         
@@ -82,25 +78,25 @@ final class ReflectionViewController: UIViewController {
         // daily, weekly 구분
         switch mission {
         case .daily:
-            missionType.text = "일일 미션"
-            missionType.backgroundColor = UIColor(named: "PointPink")
+            missionTypeLabel.text = "일일 미션"
+            missionTypeLabel.backgroundColor = UIColor(named: "PointPink")
         case .weekly:
-            missionType.text = "주간 미션"
-            missionType.backgroundColor = UIColor(named: "PointBlue")
+            missionTypeLabel.text = "주간 미션"
+            missionTypeLabel.backgroundColor = UIColor(named: "PointBlue")
         }
         
         // 각 UILabel의 텍스트 설정
         missionLabel.text = "최대글자최대글자최대글자최대글자최대글자최대글자최대글자최대글자"
         reflectionLabel.text = "최대글자수50자최대글자수50자최대글자수50자최대글자수50자최대글자수50자최대글자수50자최대"
-        informationLabel.text = "정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보"
+        missionInformationLabel.text = "정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보정보"
         
         // 행간격 조절
         reflectionLabel.setLineSpacing(lineSpacing: 4)
-        informationLabel.setLineSpacing(lineSpacing: 4)
+        missionInformationLabel.setLineSpacing(lineSpacing: 4)
     }
     
     
-    // MARK: - Test: 데이터 전달 받을 시 UI 생성
+    // TODO: 테이블 뷰에서 데이터를 전달 받아 뷰를 그리는 메소드입니다. 이후 꼬마와 데이터를 어떻게 전달받을 지 상의가 끝나면 이 메소드를 수정하여 configureUI()로 대체할 예정입니다.
 //    private func testConfigureUI() {
 //        view.backgroundColor = #colorLiteral(red: 0.9725491405, green: 0.9725491405, blue: 0.9725491405, alpha: 1)
 //
@@ -157,3 +153,4 @@ extension UILabel {
         self.attributedText = attributedString
     }
 }
+// 출처: https://stackoverflow.com/questions/39158604/how-to-increase-line-spacing-in-uilabel-in-swift
