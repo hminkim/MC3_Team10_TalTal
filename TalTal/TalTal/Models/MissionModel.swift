d13c70341d6c4e0b8d75ce6fbce3486592473589
Hyeonmin Kim <hminkim2@gmail.com>
2022-07-27 17:01:01 +0900

    [Modify] 인덴트 수정

--- TalTal/TalTal/Models/MissionModel.swift
+++ TalTal/TalTal/Models/MissionModel.swift
@@ -9,6 +9,6 @@ import Foundation
 
 // 일단 동작을 위한 모델 생성
 struct Mission {
-    let mission: String
-    var missionClearDate: String // 나중에 Date 타입으로 바꿔야 함
+	let mission: String
+	var missionClearDate: String // 나중에 Date 타입으로 바꿔야 함
 }
--- TalTal/TalTal/Views/HistoryCell.swift
+++ TalTal/TalTal/Views/HistoryCell.swift
@@ -8,19 +8,19 @@
 import UIKit
 
 class HistoryCell: UITableViewCell {
-    
-    @IBOutlet weak var cellView: UIView!
-    @IBOutlet weak var missionLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
+	
+	@IBOutlet weak var cellView: UIView!
+	@IBOutlet weak var missionLabel: UILabel!
+	@IBOutlet weak var dateLabel: UILabel!
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
 		// Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+	}
+	
+	override func setSelected(_ selected: Bool, animated: Bool) {
+		super.setSelected(selected, animated: animated)
+		
+		// Configure the view for the selected state
+	}
 }
