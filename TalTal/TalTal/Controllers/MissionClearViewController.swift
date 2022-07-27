8ccb69b117c30821597963b19fc9e45a39b525d4
E-know <eknow25@gmail.com>
2022-07-27 13:03:36 +0900

    [Feat] Mission Clear View

--- TalTal/TalTal/Controllers/MissionClearViewController.swift
+++ TalTal/TalTal/Controllers/MissionClearViewController.swift
@@ -7,24 +7,42 @@
 
 
 import UIKit
+import SwiftUI
 
 class MissionClearViewController: UIViewController {
-
+	@IBOutlet var lblMissionType: UILabel!
+	@IBOutlet var viewReflection: UIView!
+	@IBOutlet var textViewReflection: UITextView!
+	@IBOutlet var lblReflectionCount: UILabel!
+	private var isTextViewSelected: Bool = true
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		
+		view.backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
+		initblblMissionType()
+		initReflection()
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	
+	
+	private func initblblMissionType() {
+		lblMissionType.clipsToBounds = true
+		lblMissionType.layer.cornerRadius = 13
+		view.addSubview(lblMissionType)
+	}
+	
+	private func initReflection() {
+		viewReflection.clipsToBounds = true
+		viewReflection.backgroundColor = .white
+		viewReflection.layer.cornerRadius = 15
+		view.addSubview(viewReflection)
+		
+		textViewReflection.clipsToBounds = true
+		textViewReflection.backgroundColor = .clear
+		viewReflection.addSubview(textViewReflection)
+		
+		viewReflection.addSubview(lblReflectionCount)
+	}
 }
+
