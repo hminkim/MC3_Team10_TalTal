6b69c6befe338a84f48822b51903718e6eac743d
E-know <eknow25@gmail.com>
2022-07-27 15:35:22 +0900

    [Refactor] TextViewDelegate 함수 Extension으로 리팩토링

---
+++ TalTal/TalTal/StoryBoard/TextViewReflectionView.swift
@@ -0,0 +1,20 @@
+//
+//  TextViewReflectionView.swift
+//  TalTal
+//
+//  Created by Inho Choi on 2022/07/27.
+//
+
+import SwiftUI
+
+struct TextViewReflectionView: View {
+    var body: some View {
+		Color.blue
+    }
+}
+
+struct TextViewReflectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextViewReflectionView()
+    }
+}
