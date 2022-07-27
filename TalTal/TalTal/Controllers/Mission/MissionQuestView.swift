0e7c219bc9e4d45ee7aadbe7a7daeb9bb79367a4
RuyHa <code.ruyha@gmail.com>
2022-07-27 15:02:24 +0900

    StingColor중간커밋

--- TalTal/TalTal/Controllers/Mission/MissionQuestView.swift
+++ TalTal/TalTal/Controllers/Mission/MissionQuestView.swift
@@ -19,8 +19,8 @@ final class MissionQuestView: UIView {
     
     @IBOutlet weak var questLabel: UILabel!
     
-    @IBAction func questBtn(_ sender: Any) {
-    }
+    @IBOutlet weak var questBtn: UIButton!
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -36,32 +36,80 @@ final class MissionQuestView: UIView {
         guard let view = self.loadViewFromNib(nibName: "MissionQuestView") else { return }
         view.frame = self.bounds
         self.addSubview(view)
+        
     }
     
+    
     func configureView(type: MissionQuest, quest: String){
-        self.titleLabel.text = setTitle(type: type)
         self.questLabel.text = quest
         self.backgroundColor = setBackgroundColor(type: type)
+        questBtnSetting(type: type)
+        titleLabelSetting(type: type)
+        questLabelSetting()
     }
 }
 
+//MARK: 기본셋팅에 관련된 함수
 extension MissionQuestView{
     
-    func setTitle(type: MissionQuest) -> String{
+    func questBtnSetting(type: MissionQuest){
+        self.questBtn.setTitle("미션완료", for: .normal)
+        self.questBtn.setTitleColor(.white, for: .normal)
+        self.questBtn.backgroundColor = setBtnColor(type: type)
+        self.questBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+        self.questBtn.layer.cornerRadius = 14
+    }
+    
+    func titleLabelSetting(type: MissionQuest){
+        self.titleLabel.text = setTitleLabelText(type: type)
+        self.titleLabel.textColor = setTitleLabeTextlColor(type: type)
+    }
+    
+    func questLabelSetting(){
+        self.questLabel.font = UIFont.boldSystemFont(ofSize: 22)
+    }
+    
+    
+}
+
+
+//MARK: 재사용 관련된 함수모음
+extension MissionQuestView{
+    
+    func setTitleLabelText(type: MissionQuest) -> String{
         switch type{
         case.daily:
             return "오늘 해봐요"
         case.weekly:
-            return "이번주에 해봐요."
+            return "이번주에 해봐요"
         }
     }
     
-     func setBackgroundColor(type: MissionQuest) -> UIColor{
+    func setTitleLabeTextlColor(type: MissionQuest) -> UIColor{
         switch type{
         case.daily:
-            return .red
+            return UIColor(hex: "FF8166")
         case.weekly:
-            return .blue
+            return UIColor(hex: "6261F8")
+        }
+    }
+    
+    
+    func setBackgroundColor(type: MissionQuest) -> UIColor{
+        switch type{
+        case.daily:
+            return UIColor(hex: "FFF6F4")
+        case.weekly:
+            return UIColor(hex: "F4F4FF")
+        }
+    }
+    
+    func setBtnColor(type: MissionQuest) -> UIColor{
+        switch type{
+        case.daily:
+            return UIColor(hex: "FF8166")
+        case.weekly:
+            return UIColor(hex: "6261F8")
         }
     }
 }
--- TalTal/TalTal/Controllers/Mission/MissionViewController.swift
+++ TalTal/TalTal/Controllers/Mission/MissionViewController.swift
@@ -8,28 +8,62 @@
 
 import UIKit
 
+
 class MissionViewController: UIViewController {
 
     @IBOutlet weak var dailyView: MissionQuestView!
     @IBOutlet weak var weeklyView: MissionQuestView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    let viewCornerRadius = 20.0
+    var dailyClearQuest = 0
+    var weeklyClearQuest = 0
+    var questText = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.dailyView.configureView(type: .daily, quest: "깔깔깔갈갈갈갈ㅇㅇㅇㅇ갈갉깔깔깔갈ㅇㅇㅇㅇㅇ갈갈갈갈")
-        self.weeklyView
-            .configureView(type: .weekly, quest: "가나다라마바사아자차카타바파하아카카ㅏ캌")
+        settingQuestView()
+        questText = "지금까지\n\(dailyClearQuest)개의 일일 미션과\n\(weeklyClearQuest)주간 미션을 완료했어요!"
+        var p1 = ppap(text: String(dailyClearQuest), color: UIColor(hex: "FF8166"))
+        var p2 = ppap(text: "일일 미션과", color: UIColor(hex: "FF8166"))
+        var p3 = ppap(text: String(weeklyClearQuest), color: UIColor(hex: "6261F8"))
+        var p4 = ppap(text: "주간 미션", color: UIColor(hex: "6261F8"))
+//        questText = "지금까지\n\(p1)개의 \(p2)\n\(p3)개의 주간 미션을 완료했어요!"
+        textLabel.text = questText
+        
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension MissionViewController{
+    func settingQuestView(){
+        self.dailyView.configureView(type: .daily, quest: "깔깔깔갈갈갈갈ㅇㅇㅇㅇ갈갉깔깔깔갈ㅇㅇㅇㅇㅇ갈갈갈갈")
+        self.weeklyView
+            .configureView(type: .weekly, quest: "가나다라마바사아자차카타바파하아카카ㅏ캌")
+        dailyView.layer.cornerRadius = viewCornerRadius
+        weeklyView.layer.cornerRadius = viewCornerRadius
+
     }
-    */
-
+    
+//
+//    func partialColorString(allString: String,allStringColor: UIColor ,partialString: String, partialStringColor: UIColor ) -> AttributedString {
+//        var string = AttributedString(allString)
+//        string.foregroundColor = allStringColor
+//
+//        if let range = string.range(of: partialString) {
+//            string[range].foregroundColor = partialStringColor
+//        }
+//        return string
+//    }
+    
+    func ppap(text: String, color: UIColor)->AttributedString{
+    var string = AttributedString(text)
+    string.foregroundColor = color
+    return string
+    }
 }
