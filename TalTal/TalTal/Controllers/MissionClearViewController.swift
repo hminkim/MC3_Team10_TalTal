c01c54e580291008fdbf7557cc9185c5a05bf53b
E-know <eknow25@gmail.com>
2022-07-27 16:28:58 +0900

    [Modify] 연속 엔터 입력 방지 및 Delegate 리팩토링

--- TalTal/TalTal/Controllers/MissionClearViewController.swift
+++ TalTal/TalTal/Controllers/MissionClearViewController.swift
@@ -9,56 +9,80 @@
 import UIKit
 import SwiftUI
 
-class MissionClearViewController: UIViewController, UITextViewDelegate {
-	@IBOutlet var lblMissionType: UILabel!
-	@IBOutlet var viewReflection: UIView!
-	@IBOutlet var textViewReflection: UITextView!
-	@IBOutlet var lblReflectionCount: UILabel!
-	@IBOutlet var btnConfirm: UIButton!
-	@IBOutlet var btnCancel: UIButton!
+class MissionClearViewController: UIViewController {
+
+	//MARK: 마크마크
+	@IBOutlet var missionTypeLabel: UILabel!
+	@IBOutlet var reflectionView: UIView!
+	@IBOutlet var reflectionTextView: UITextView!
+	@IBOutlet var reflectionTextCountLable: UILabel!
+	@IBOutlet var confirmButton: UIButton!
+	@IBOutlet var cancelButton: UIButton!
 	private var isTextViewSelected: Bool = true
-	
-    override func viewDidLoad() {
-        super.viewDidLoad()
-		
-		textViewReflection.delegate = self
-		view.backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		reflectionTextView.delegate = self
+		view.backgroundColor = UIColor(red: 248 / 255, green: 248 / 255, blue: 248 / 255, alpha: 1)
 		initblblMissionType()
 		initReflection()
-        // Do any additional setup after loading the view.
-    }
-	
-	
+		// Do any additional setup after loading the view.
+	}
+
 	private func initblblMissionType() {
-		lblMissionType.clipsToBounds = true
-		lblMissionType.layer.cornerRadius = 13
-		view.addSubview(lblMissionType)
+		missionTypeLabel.clipsToBounds = true
+		missionTypeLabel.layer.cornerRadius = 13
+		view.addSubview(missionTypeLabel)
 	}
-	
+
 	private func initReflection() {
-		viewReflection.clipsToBounds = true
-		viewReflection.backgroundColor = .white
-		viewReflection.layer.cornerRadius = 15
-		view.addSubview(viewReflection)
-		
-		textViewReflection.clipsToBounds = true
-		textViewReflection.backgroundColor = .clear
-		viewReflection.addSubview(textViewReflection)
-		
-		viewReflection.addSubview(lblReflectionCount)
+		reflectionView.clipsToBounds = true
+		reflectionView.backgroundColor = .white
+		reflectionView.layer.cornerRadius = 15
+		view.addSubview(reflectionView)
+
+		reflectionTextView.clipsToBounds = true
+		reflectionTextView.backgroundColor = .clear
+		reflectionView.addSubview(reflectionTextView)
+		reflectionView.addSubview(reflectionTextCountLable)
 	}
-	
+
+	@IBAction func actionBtnCancel(_ sender: Any) {
+		//TODO: Close Modal
+		//BUG:
+		//FIXME: 이거 고쳐야함
+	}
+
+	@IBAction func actionBtnConfirm(_ sender: Any) {
+		//TODO: Save Reflection & Close Modal
+	}
+}
+
+extension MissionClearViewController: UITextViewDelegate {
 	func textViewDidChange(_ textView: UITextView) {
+		// 연속으로 Enter 입력시 방지
+		if textView.text.last == "\n" {
+			let index = textView.text.index(textView.text.endIndex, offsetBy: -2)
+			if textView.text[index] == "\n" {
+				let start = textView.text.startIndex
+				let end = textView.text.index(textView.text.endIndex, offsetBy: -1)
+				let range = start..<end
+				textView.text = String(textView.text[range])
+			}
+		}
+
+// 50 글자 이상 방지
 		if textView.text.count > 50 {
 			let start = textView.text.startIndex
 			let end = textView.text.index(textView.text.startIndex, offsetBy: 50)
 			let range = start..<end
 			textView.text = String(textView.text[range])
 		}
-		
-		lblReflectionCount.text = "\(textView.text.count) / 50"
+
+		reflectionTextCountLable.text = "\(textView.text.count) / 50"
 	}
-	
+
 	func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
 		if isTextViewSelected {
 			textView.text = ""
@@ -67,24 +91,13 @@ class MissionClearViewController: UIViewController, UITextViewDelegate {
 		}
 		return true
 	}
-	
+
+
 	func textViewDidEndEditing(_ textView: UITextView) {
 		if textView.text.count == 0 {
 			isTextViewSelected = true
 			textView.text = "50자 이내로 적어주세요."
-			textView.textColor = UIColor(red: 199/255, green: 199/255, blue: 204/255, alpha: 1)
+			textView.textColor = UIColor(red: 199 / 255, green: 199 / 255, blue: 204 / 255, alpha: 1)
 		}
 	}
-	
-	@IBAction func actionBtnCancel(_ sender: Any) {
-		//TODO: Close Modal
-	}
-	
-	
-	@IBAction func actionBtnConfirm(_ sender: Any) {
-		//TODO: Save Reflection & Close Modal
-	}
-	
-	
 }
-
